98d986df5692c3fdda50827bf0243bc88ef74a09
JGoujon <j.jeanguillaume@gmail.com>
2024-07-14 18:20:32 +0200

    plug fade in system to game

--- Assets/ReloadScene.cs
+++ Assets/ReloadScene.cs
@@ -5,12 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator fadeAnimator;  // Référence à l'Animator
+    public float fadeDuration = 6f; // Durée de l'animation de fade
+    public Canvas canvas;
+
+    private void Start()
+    {
+        canvas.enabled = false;
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("Player entered the trigger.");  // Log pour vérifier l'entrée du joueur
+            if (fadeAnimator != null && fadeAnimator.isActiveAndEnabled)
+            {
+                canvas.enabled = !canvas.enabled;
+                StartCoroutine(ReloadAfterFade());
+            }
+            else
+            {
+                Debug.Log("Fade Animator is not properly set up.");
+            }
         }
+    }
+
+    private IEnumerator ReloadAfterFade()
+    {
+        Debug.Log("Fade animation triggered.");  // Log pour vérifier le déclenchement de l'animation
+        fadeAnimator.SetTrigger("Fade");  // Déclencher l'animation de fade
+        yield return new WaitForSeconds(fadeDuration); // Attendre la fin de l'animation
+        //canvasGroup.alpha = 1;
+        Debug.Log("Scene is reloading.");  // Log juste avant de recharger la scène
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
