b6e4190aefd4d98fd07747d2e56eb44368ca36fb
JGoujon <j.jeanguillaume@gmail.com>
2024-09-15 13:52:07 +0200

    add music & phaser + chorus on death

--- Assets/ReloadScene.cs
+++ Assets/ReloadScene.cs
@@ -1,3 +1,5 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,12 +14,21 @@ public class ReloadScene : MonoBehaviour
     public Canvas canvas;
     public Canvas canvasTooBad;
     public Canvas canvasSmoke;
+    public string music;
+    public string snapshot;
+    public FMOD.Studio.EventInstance instance;
+    private FMOD.Studio.EventInstance  muteaudio;
 
     private void Start()
     {
         canvas.enabled = false;
         canvasTooBad.enabled = false;
         canvasSmoke.enabled = false;
+
+        instance = FMODUnity.RuntimeManager.CreateInstance(music);
+        instance.start();
+
+        muteaudio = FMODUnity.RuntimeManager.CreateInstance(snapshot);
     }
 
     private void OnTriggerEnter(Collider col)
@@ -39,15 +50,36 @@ public class ReloadScene : MonoBehaviour
         }
     }
 
+    void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            // Stop the snapshot to restore normal audio when the application gains focus
+            muteaudio.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+        else
+        {
+            // Start the snapshot to mute audio when the application loses focus
+            muteaudio.start();
+        }
+    }
+
     private IEnumerator ReloadAfterFade()
     {
         Debug.Log("Fade animation triggered.");  // Log pour vérifier le déclenchement de l'animation
         fadeAnimator.SetTrigger("Fade");  // Déclencher l'animation de fade
         tooBadAnimator.SetTrigger("PlayTooBad");
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         //smokeEffect.Play();
         yield return new WaitForSeconds(fadeDuration); // Attendre la fin de l'animation
         //canvasGroup.alpha = 1;
         Debug.Log("Scene is reloading.");  // Log juste avant de recharger la scène
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnDestroy()
+    {
+        instance.release();
+        muteaudio.release();
     }
 }
