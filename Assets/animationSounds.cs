c1d0da33c3e62f8cdbfc24e1c5629b1cc85d154c
JGoujon <j.jeanguillaume@gmail.com>
2024-10-06 19:26:38 +0200

    add FS + change Land name in Animation Window

--- Assets/animationSounds.cs
+++ Assets/animationSounds.cs
@@ -6,8 +6,13 @@ public class animationSounds : MonoBehaviour
 {
     // Start is called before the first frame update
     public string playerLand;
-    public void PlayFMODSound()
+    public string playerWalk;
+    public void Player_Land()
     {
         FMODUnity.RuntimeManager.PlayOneShot(playerLand, transform.position);
+    }
+    public void Player_Walk()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(playerWalk, transform.position);
     }
 }
