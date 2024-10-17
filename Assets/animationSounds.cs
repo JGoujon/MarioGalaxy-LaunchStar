e2a9c79fb8f5b38350503b5002553628ab070e98
JGoujon <j.jeanguillaume@gmail.com>
2024-10-17 22:05:18 +0200

    add walk only if character grounded

--- Assets/animationSounds.cs
+++ Assets/animationSounds.cs
@@ -8,18 +8,26 @@ public class animationSounds : MonoBehaviour
     public string playerLand;
     public string playerWalk;
     private bool isWalking = false;
+    private CharacterController characterController;
+
     public void Player_Land()
     {
         FMODUnity.RuntimeManager.PlayOneShot(playerLand, transform.position);
     }
     public void Player_Walk()
     {
-        if (isWalking) 
+        if (isWalking && characterController.isGrounded) 
         { 
             FMODUnity.RuntimeManager.PlayOneShot(playerWalk, transform.position);
         }
         
     }
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         // Check if player is moving
